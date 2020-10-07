080ca40185f5314e6c8088f5517455f701e7d8b0
Ethan Brown <ewb@ewb.io>
2020-10-07 16:47:37 +0200

    fixed scrape.py

--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-import urllib
+from urllib.request import urlopen
 
 import bs4 as bs
 import pandas as pd
@@ -10,7 +10,7 @@ def get_last_statement(uri):
         url = domain + uri
     else:
         url = domain + r"/death_row/" + uri
-    soup = bs.BeautifulSoup(urllib.urlopen(url).read(), "html.parser")
+    soup = bs.BeautifulSoup(urlopen(url).read(), "html.parser")
     ps = soup.find("div", {"id": "content_right"}).find_all("p")
     count = 0
     for p in ps:
@@ -18,6 +18,8 @@ def get_last_statement(uri):
         if "Last Statement:" in p:
             break
         count += 1
+    if count + 1 > len(ps) - 1:
+        return None
     last_statement = " ".join("".join(ps[count + 1].stripped_strings).split())
     print(last_statement)
     return last_statement
@@ -25,7 +27,7 @@ def get_last_statement(uri):
 
 def main():
     url = r"http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html"
-    soup = bs.BeautifulSoup(urllib.urlopen(url).read(), "html.parser")
+    soup = bs.BeautifulSoup(urlopen(url).read(), "html.parser")
     parsed_table = soup.find_all("table")[0]
     data = [
         [td.a["href"] if td.find("a") else "".join(td.stripped_strings) for td in row.find_all("td")]
@@ -35,6 +37,7 @@ def main():
     df = pd.DataFrame(data[1:], columns=headers)
     df.columns = pd.io.parsers.ParserBase({"names": df.columns})._maybe_dedup_names(df.columns)
     df["Last Statement"] = df["Link.1"].apply(get_last_statement)
+    df = df[~df["Last Statement"].isnull()]
     print(df)
     df.to_csv("last.csv", encoding="utf-8")
 
