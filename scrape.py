6b90218c64250157be0ac2c9ff48f6d02b5db5f2
Ethan Brown <ewb@ewb.io>
2020-10-07 15:30:48 +0200

    update pre-commit

--- scrape.py
+++ scrape.py
@@ -4,18 +4,6 @@ import bs4 as bs
 import pandas as pd
 
 
-url = r"http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html"
-soup = bs.BeautifulSoup(urllib.urlopen(url).read(), "html.parser")
-parsed_table = soup.find_all("table")[0]
-data = [
-    [td.a["href"] if td.find("a") else "".join(td.stripped_strings) for td in row.find_all("td")]
-    for row in parsed_table.find_all("tr")
-]
-headers = ["".join(th.stripped_strings) for th in parsed_table.find_all("th")]
-df = pd.DataFrame(data[1:], columns=headers)
-df.columns = pd.io.parsers.ParserBase({"names": df.columns})._maybe_dedup_names(df.columns)
-
-
 def get_last_statement(uri):
     domain = r"http://www.tdcj.state.tx.us"
     if uri[0] == "/":
@@ -35,6 +23,21 @@ def get_last_statement(uri):
     return last_statement
 
 
-df["Last Statement"] = df["Link.1"].apply(get_last_statement)
-print(df)
-df.to_csv("last.csv", encoding="utf-8")
+def main():
+    url = r"http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html"
+    soup = bs.BeautifulSoup(urllib.urlopen(url).read(), "html.parser")
+    parsed_table = soup.find_all("table")[0]
+    data = [
+        [td.a["href"] if td.find("a") else "".join(td.stripped_strings) for td in row.find_all("td")]
+        for row in parsed_table.find_all("tr")
+    ]
+    headers = ["".join(th.stripped_strings) for th in parsed_table.find_all("th")]
+    df = pd.DataFrame(data[1:], columns=headers)
+    df.columns = pd.io.parsers.ParserBase({"names": df.columns})._maybe_dedup_names(df.columns)
+    df["Last Statement"] = df["Link.1"].apply(get_last_statement)
+    print(df)
+    df.to_csv("last.csv", encoding="utf-8")
+
+
+if __name__ == "__main__":
+    main()
